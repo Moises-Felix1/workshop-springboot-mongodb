d701cb1835d1a26f2f244ae6ff7139e3deb76216
Moises Felix <moises012felix@gmail.com>
2024-08-03 15:27:30 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/moisesfelix/workshopmongodb1/Workshopmongodb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moisesfelix.workshopmongodb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongodb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
