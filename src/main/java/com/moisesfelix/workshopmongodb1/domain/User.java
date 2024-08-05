4a713640b7fdc33489124411bf514fa1064523e4
Moises Felix <moises012felix@gmail.com>
2024-08-05 10:41:29 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/moisesfelix/workshopmongodb1/domain/User.java
+++ src/main/java/com/moisesfelix/workshopmongodb1/domain/User.java
@@ -18,10 +18,10 @@ public class User implements Serializable {
 	public User() {
 	}
 
-	public User(String name, String id, String email) {
+	public User(String id, String name, String email) {
 		super();
-		this.name = name;
 		this.id = id;
+		this.name = name;
 		this.email = email;
 	}
 
