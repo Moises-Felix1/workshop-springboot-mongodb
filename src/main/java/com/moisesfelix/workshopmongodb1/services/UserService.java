e860f06379ad0d9e06534efada5801a200d2ad36
Moises Felix <moises012felix@gmail.com>
2024-08-14 14:46:34 -0300

    Atualização de usuário com PUT

--- src/main/java/com/moisesfelix/workshopmongodb1/services/UserService.java
+++ src/main/java/com/moisesfelix/workshopmongodb1/services/UserService.java
@@ -30,11 +30,22 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
-	public void deleteBy(String id) {
+	public void deleteById(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User FromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
