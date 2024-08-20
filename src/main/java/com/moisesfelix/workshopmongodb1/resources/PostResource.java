b552483027041e1c7c33ec73a6d16c4c31a20bb1
Moises Felix <moises012felix@gmail.com>
2024-08-20 10:23:57 -0300

    Consulta com vários critérios

--- src/main/java/com/moisesfelix/workshopmongodb1/resources/PostResource.java
+++ src/main/java/com/moisesfelix/workshopmongodb1/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.moisesfelix.workshopmongodb1.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +33,18 @@ public class PostResource {
  	public ResponseEntity<List<Post>> Title(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/fullsearch")
+ 	public ResponseEntity<List<Post>> fullsearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="min", defaultValue="") String min,
+ 			@RequestParam(value="max", defaultValue="") String max) {
+		text = URL.decodeParam(text);
+		Date minDate = URL.covertDate(min, new Date(0L));
+		Date maxDate = URL.covertDate(max, new Date());
+		List<Post> list = service.fullsearch(text, minDate, maxDate);
 		return ResponseEntity.ok().body(list);
 	}
 }
